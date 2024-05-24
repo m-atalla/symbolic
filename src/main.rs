d8583e939576705251c540116394e622f06ce49b
m-atalla <mo.atalla96@gmail.com>
2024-05-24 23:30:41 +0300

    add update subcommand alias + fmt

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,50 @@
 use std::process;
 
-use clap::{Parser, Subcommand, Args};
+use clap::{Args, Parser, Subcommand};
 /// Configuration files symbolic linking utility
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    command: Commands, 
-    #[arg(default_value="$HOME")]
+    command: Commands,
+    #[arg(default_value = "$HOME")]
     home: Option<String>,
 }
-
 
 #[derive(Subcommand)]
 enum Commands {
     /// links file from `<SOURCE> to -> <TARGET>`
     Link { source: String, target: String },
     /// update symbolic links for the provided `.sym` file path. (default=".")
+    #[clap(visible_alias("up"))]
     Update(PathArg),
     /// breaks target links for the provided `.sym` file path. (default=".")
-    Break(PathArg) 
+    Break(PathArg),
 }
-
 
 #[derive(Args, Debug)]
 struct PathArg {
-    path: Option<String>
+    path: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Link{ source, target } => { 
-            if let Err(err) = symbolic::link_up(source, target) { 
+        Commands::Link { source, target } => {
+            if let Err(err) = symbolic::link_up(source, target) {
                 eprintln!("{}", err);
                 process::exit(1);
             }
-        },
-        Commands::Update(_arg) => { 
+        }
+        Commands::Update(_arg) => {
             if let Err(err) = symbolic::run() {
                 eprintln!("{}", err);
                 process::exit(1);
             }
-        },
-        Commands::Break(_arg) => { 
+        }
+        Commands::Break(_arg) => {
+            println!("path: {:?}", _arg);
             unimplemented!()
         }
     }
